61e0b7f42a01d0c62ffcd199f8190c4732747f61
Luo Yu <blodely@gmail.com>
2013-11-01 17:03:13 +0800

    ADD: user default category

---
+++ LYCategory/_Foundation/NSUserDefaults+Speed.h
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+Speed.h
+//  LYCategory
+//
+//  Created by Rick Luo on 12/5/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (Speed)
+
+@end
---
+++ LYCategory/_Foundation/NSUserDefaults+Speed.m
@@ -0,0 +1,13 @@
+//
+//  NSUserDefaults+Speed.m
+//  LYCategory
+//
+//  Created by Rick Luo on 12/5/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import "NSUserDefaults+Speed.h"
+
+@implementation NSUserDefaults (Speed)
+
+@end
