c9a17cce11369e13824703878157b8e38a095d3d
Luo Yu <blodely@gmail.com>
2013-12-18 20:20:52 -0600

    ADD: text demo view

---
+++ LYCategorySample/LYCategory/_Scene/Text/TextViewController.h
@@ -0,0 +1,13 @@
+//
+//  TextViewController.h
+//  LYCategory
+//
+//  Created by Rick Luo on 12/18/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextViewController : UIViewController
+
+@end
---
+++ LYCategorySample/LYCategory/_Scene/Text/TextViewController.m
@@ -0,0 +1,51 @@
+//
+//  TextViewController.m
+//  LYCategory
+//
+//  Created by Rick Luo on 12/18/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import "TextViewController.h"
+
+@interface TextViewController ()
+
+@end
+
+@implementation TextViewController
+
+#pragma mark - ACTIONS
+
+#pragma mark - INIT
+
+- (id)init {
+	self = [super initWithNibName:@"TextViewController" bundle:nil];
+	if (self) {
+		// CUSTOM INITIALIZATION
+	}
+	return self;
+}
+
+#pragma mark VIEW LIFE CYCLE
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// DO ANY ADDITIONAL SETUP AFTER LOADING THE VIEW FROM ITS NIB.
+}
+
+#pragma mark MEMORY MANAGEMENT
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// DISPOSE OF ANY RESOURCES THAT CAN BE RECREATED.
+}
+
+#pragma mark - GETTER & SETTER
+
+#pragma mark - METHOD
+
+#pragma mark PRIVATE METHOD
+
+#pragma mark - DELEGATE
+
+@end
