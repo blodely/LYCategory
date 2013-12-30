e81f9ad785c6918be160ec763bd408b069aefc69
Luo Yu <blodely@gmail.com>
2013-12-30 17:19:13 +0800

    ADD: search string

---
+++ LYCategory/_Foundation/_Work/NSString+Search.h
@@ -0,0 +1,14 @@
+//
+//  NSString+Search.h
+//  LYCategory
+//
+//  Created by Rick Luo on 12/29/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//	Email: indie.luo@gmail.com
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Search)
+
+@end
---
+++ LYCategory/_Foundation/_Work/NSString+Search.m
@@ -0,0 +1,14 @@
+//
+//  NSString+Search.m
+//  LYCategory
+//
+//  Created by Rick Luo on 12/29/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//	Email: indie.luo@gmail.com
+//
+
+#import "NSString+Search.h"
+
+@implementation NSString (Search)
+
+@end
