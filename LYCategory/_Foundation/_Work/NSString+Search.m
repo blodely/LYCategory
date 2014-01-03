616d289b9809a917dd14ea099e46cf05f638bff8
Luo Yu <blodely@gmail.com>
2014-01-03 17:25:29 +0800

    MODIFY: search

--- LYCategory/_Foundation/_Work/NSString+Search.m
+++ LYCategory/_Foundation/_Work/NSString+Search.m
@@ -13,7 +13,12 @@
 
 - (BOOL)caseInsensitive:(NSString *)searchString {
 	
-	return NO;
+	if ([searchString length] == 0 || [self length] == 0 ||
+		([searchString length] > 0 && [self length] > 0 && searchString.length > self.length)) {
+		return NO;
+	}
+	
+	return [[self lowercaseString] rangeOfString:[searchString lowercaseString]].location != NSNotFound;
 }
 
 @end
