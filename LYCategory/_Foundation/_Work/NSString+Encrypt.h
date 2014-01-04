c6419b7297fcf3462355c3ad7b12ad7634aca60b
Luo Yu <blodely@gmail.com>
2014-01-04 17:17:20 +0800

    ADD: encrypt string

---
+++ LYCategory/_Foundation/_Work/NSString+Encrypt.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Encrypt.h
+//  LYCategory
+//
+//  Created by Rick Luo on 1/4/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Encrypt)
+
+@end
---
+++ LYCategory/_Foundation/_Work/NSString+Encrypt.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Encrypt.m
+//  LYCategory
+//
+//  Created by Rick Luo on 1/4/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSString+Encrypt.h"
+
+@implementation NSString (Encrypt)
+
+@end
