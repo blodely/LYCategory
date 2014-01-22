8d9c5e2d87da66c2bb50e0760e7117ad19335f4d
Luo Yu <blodely@gmail.com>
2014-01-22 17:17:40 +0800

    ADD: UILabel+Extra

---
+++ LYCategory/_UIKit/UILabel+Extra.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extra.h
+//  LYCategory
+//
+//  Created by Rick Luo on 1/22/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Extra)
+
+@end
---
+++ LYCategory/_UIKit/UILabel+Extra.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+Extra.m
+//  LYCategory
+//
+//  Created by Rick Luo on 1/22/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "UILabel+Extra.h"
+
+@implementation UILabel (Extra)
+
+@end
