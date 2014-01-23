61dd5201393423ecd283cef435908a1ab2ed6e59
Luo Yu <blodely@gmail.com>
2014-01-23 17:17:30 +0800

    ADD: macro for iOS

---
+++ LYCategory/_UIKit/MacroIOS.h
@@ -0,0 +1,14 @@
+//
+//  MacroIOS.h
+//  LYCategory
+//
+//  Created by Rick Luo on 1/23/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#ifndef LYCategory_MacroIOS_h
+#define LYCategory_MacroIOS_h
+
+
+
+#endif
