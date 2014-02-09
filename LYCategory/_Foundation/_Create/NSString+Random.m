abe0da2ff4928e0f8ec2f7d8d7c1581d559839fa
Luo Yu <indie.luo@gmail.com>
2014-02-09 17:19:34 +0800

    random string generator m file

---
+++ LYCategory/_Foundation/_Create/NSString+Random.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Random.m
+//  LYCategory
+//
+//  Created by Luo Yu on 2014.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSString+Random.h"
+
+@implementation NSString (Random)
+
+@end
