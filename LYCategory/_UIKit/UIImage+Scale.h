9db9d4eae512f171c66c0b22c7a328731d0c7353
Luo Yu <indie.luo@gmail.com>
2014-02-24 17:18:42 +0800

    add UIImage scale category header file

---
+++ LYCategory/_UIKit/UIImage+Scale.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+Scale.h
+//  LYCategory
+//
+//  Created by Luo Yu on 2/24/14.
+//  Email : indie.luo@gmail.com
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Scale)
+
+@end
