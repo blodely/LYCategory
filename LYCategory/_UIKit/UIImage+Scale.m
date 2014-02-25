53d13e3fd8e844a9e3a8bc36f32625d29dfdc071
Luo Yu <indie.luo@gmail.com>
2014-02-25 17:19:19 +0800

    add UIImage scale category implementation file

---
+++ LYCategory/_UIKit/UIImage+Scale.m
@@ -0,0 +1,14 @@
+//
+//  UIImage+Scale.m
+//  LYCategory
+//
+//  Created by Luo Yu on 2/24/14.
+//  Email : indie.luo@gmail.com
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "UIImage+Scale.h"
+
+@implementation UIImage (Scale)
+
+@end
