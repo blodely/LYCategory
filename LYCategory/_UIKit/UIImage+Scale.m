198ed099dc972dca714f6acd4d5a1e7a198d1af5
Luo Yu <indie.luo@gmail.com>
2014-03-07 17:17:46 +0800

    implementation

--- LYCategory/_UIKit/UIImage+Scale.m
+++ LYCategory/_UIKit/UIImage+Scale.m
@@ -13,7 +13,34 @@
 
 // RESIZE IMAGE
 - (UIImage *)resize:(CGSize)size {
-	return nil;
+	
+	CGFloat actualHeight = self.size.height;
+	CGFloat actualWidth = self.size.width;
+	if (actualWidth <= size.width && actualHeight<=size.height) {
+		// SMALLER IMAGE
+		return self;
+	}
+	
+	float oldRatio = actualWidth/actualHeight;
+	float newRatio = size.width/size.height;
+	if (oldRatio < newRatio) {
+		oldRatio = size.height/actualHeight;
+		actualWidth = oldRatio * actualWidth;
+		actualHeight = size.height;
+	} else {
+		oldRatio = size.width/actualWidth;
+		actualHeight = oldRatio * actualHeight;
+		actualWidth = size.width;
+	}
+	
+	UIImage *image = self;
+	CGRect rect = CGRectMake(0.0,0.0,actualWidth,actualHeight);
+	UIGraphicsBeginImageContext(rect.size);
+	[image drawInRect:rect];
+	image = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	
+	return image;
 }
 
 @end
