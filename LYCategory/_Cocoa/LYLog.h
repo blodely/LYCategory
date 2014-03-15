be19e0a9a536ed7d93d61eedbbe6186004c8b7d8
Luo Yu <indie.luo@gmail.com>
2014-03-15 17:16:48 +0800

    Add log function.

---
+++ LYCategory/_Cocoa/LYLog.h
@@ -0,0 +1,13 @@
+//
+//  LYLog.h
+//  LYCategory
+//
+//  Created by 骆昱 on 2014-03-15.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYLog : NSObject
+
+@end
