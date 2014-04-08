10f33bc0b493dee820958a0b97339639779e8b62
Luo Yu <indie.luo@gmail.com>
2014-04-08 17:17:53 +0800

    file manager category for iOS device head file.

---
+++ LYCategory/_Foundation/_Work/NSFileManager+IOS.h
@@ -0,0 +1,13 @@
+//
+//  NSFileManager+IOS.h
+//  LYCategory
+//
+//  Created by Luo Yu on 4/8/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFileManager (IOS)
+
+@end
