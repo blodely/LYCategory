9bdd2cb7508304e411c36819738d6e880a03900e
Luo Yu <indie.luo@gmail.com>
2014-04-09 17:18:12 +0800

    file manager.

---
+++ LYCategory/_Foundation/_Work/NSFileManager+IOS.m
@@ -0,0 +1,13 @@
+//
+//  NSFileManager+IOS.m
+//  LYCategory
+//
+//  Created by Luo Yu on 4/8/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSFileManager+IOS.h"
+
+@implementation NSFileManager (IOS)
+
+@end
