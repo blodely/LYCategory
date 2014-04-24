8b8c6cbdeaf92b048fd7c3138aa35a63e6d3d40f
Luo Yu <indie.luo@gmail.com>
2014-04-24 22:55:36 +0800

    UIView screenshot

---
+++ LYCategory/_UIKit/UIView+Screenshot.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Screenshot.h
+//  LYCategory
+//
+//  Created by Luo Yu on 2014/03/01.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Screenshot)
+
+@end
---
+++ LYCategory/_UIKit/UIView+Screenshot.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Screenshot.m
+//  LYCategory
+//
+//  Created by Luo Yu on 2014/03/01.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "UIView+Screenshot.h"
+
+@implementation UIView (Screenshot)
+
+@end
