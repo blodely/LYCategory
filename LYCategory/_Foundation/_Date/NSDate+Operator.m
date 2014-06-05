4fb15885706dfb7fa9f1ce9ec5941e36699c4719
Luo Yu <indie.luo@gmail.com>
2014-06-05 17:17:52 +0800

    Fix : is the day before yesterday method.

--- LYCategory/_Foundation/_Date/NSDate+Operator.m
+++ LYCategory/_Foundation/_Date/NSDate+Operator.m
@@ -25,7 +25,7 @@
 }
 
 - (BOOL)isTheDayBeforeYesterday {
-	return [[self yesterday] isSameDayIgnoringTimeWith:[[[NSDate date] yesterday] yesterday]];
+	return [self isSameDayIgnoringTimeWith:[[[NSDate date] yesterday] yesterday]];
 }
 
 - (BOOL)isSameDayIgnoringTimeWith:(NSDate *)date {
