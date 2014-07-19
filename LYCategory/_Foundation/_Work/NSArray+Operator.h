5607523243237d23b405bebf309cfa21a766ab48
Luo Yu <indie.luo@gmail.com>
2014-07-19 17:19:24 +0800

    array operator

---
+++ LYCategory/_Foundation/_Work/NSArray+Operator.h
@@ -0,0 +1,15 @@
+//
+//  NSArray+Operator.h
+//  LYCategory
+//
+//  Created by 骆昱 on 7/19/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Operator)
+
+- (NSArray *)reversed;
+
+@end
---
+++ LYCategory/_Foundation/_Work/NSArray+Operator.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+Operator.m
+//  LYCategory
+//
+//  Created by Luo Yu on 7/19/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSArray+Operator.h"
+
+@implementation NSArray (Operator)
+
+@end
