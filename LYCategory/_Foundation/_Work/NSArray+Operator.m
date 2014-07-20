a76947ada027a3fc1e7ce0fc772ca05d0f7acdf4
Luo Yu <indie.luo@gmail.com>
2014-07-20 17:21:11 +0800

    implementation

--- LYCategory/_Foundation/_Work/NSArray+Operator.m
+++ LYCategory/_Foundation/_Work/NSArray+Operator.m
@@ -10,4 +10,13 @@
 
 @implementation NSArray (Operator)
 
+- (NSArray *)reversed {
+	NSMutableArray *array = [NSMutableArray arrayWithCapacity:[self count]];
+	NSEnumerator *enumerator = [self reverseObjectEnumerator];
+	for (id element in enumerator) {
+		[array addObject:element];
+	}
+	return [NSArray arrayWithArray:array];
+}
+
 @end
