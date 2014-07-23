3354ac8970035ec1c164bc01bc5e02c4bb4b4f7f
Luo Yu <indie.luo@gmail.com>
2014-07-23 17:25:25 +0800

    reverse mutable array item order

--- LYCategory/_Foundation/_Work/NSMutableArray+Operator.h
+++ LYCategory/_Foundation/_Work/NSMutableArray+Operator.h
@@ -10,4 +10,6 @@
 
 @interface NSMutableArray (Operator)
 
+- (void)reverse;
+
 @end
--- LYCategory/_Foundation/_Work/NSMutableArray+Operator.m
+++ LYCategory/_Foundation/_Work/NSMutableArray+Operator.m
@@ -10,4 +10,18 @@
 
 @implementation NSMutableArray (Operator)
 
+- (void)reverse {
+	if ([self count] == 0)
+		return;
+	NSUInteger i = 0;
+	NSUInteger j = [self count] - 1;
+	while (i < j) {
+		[self exchangeObjectAtIndex:i
+				  withObjectAtIndex:j];
+		
+		i++;
+		j--;
+	}
+}
+
 @end
