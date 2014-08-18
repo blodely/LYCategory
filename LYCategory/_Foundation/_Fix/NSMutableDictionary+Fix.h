90a4227d008ee46258ffc396a6310f323a4e47e9
Luo Yu <indie.luo@gmail.com>
2014-08-18 17:17:38 +0800

    Create mutable dictionary fix category file.

---
+++ LYCategory/_Foundation/_Fix/NSMutableDictionary+Fix.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableDictionary+Fix.h
+//  LYCategory
+//
+//  Created by Luo Yu on 8/18/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableDictionary (Fix)
+
+@end
---
+++ LYCategory/_Foundation/_Fix/NSMutableDictionary+Fix.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableDictionary+Fix.m
+//  LYCategory
+//
+//  Created by Luo Yu on 8/18/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSMutableDictionary+Fix.h"
+
+@implementation NSMutableDictionary (Fix)
+
+@end
