df3e4750c38bc4c1fe416b937037d491a11a8124
Luo Yu <indie.luo@gmail.com>
2014-08-24 17:18:01 +0800

    Create mutable array fixer.

---
+++ LYCategory/_Foundation/_Fix/NSMutableArray+Fix.h
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+Fix.h
+//  LYCategory
+//
+//  Created by Luo Yu on 8/24/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (Fix)
+
+@end
---
+++ LYCategory/_Foundation/_Fix/NSMutableArray+Fix.m
@@ -0,0 +1,13 @@
+//
+//  NSMutableArray+Fix.m
+//  LYCategory
+//
+//  Created by Luo Yu on 8/24/14.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "NSMutableArray+Fix.h"
+
+@implementation NSMutableArray (Fix)
+
+@end
