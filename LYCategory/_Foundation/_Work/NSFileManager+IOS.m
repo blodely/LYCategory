6b244ea6cc94518d3d0724c489a6c923d5fed1fb
Luo Yu <indie.luo@gmail.com>
2014-09-03 17:24:33 +0800

    implementation

--- LYCategory/_Foundation/_Work/NSFileManager+IOS.m
+++ LYCategory/_Foundation/_Work/NSFileManager+IOS.m
@@ -19,6 +19,19 @@
 }
 
 + (void)createFolderIfNotExistInDocuments:(NSString *)folderName {
+	
+	// CHECK FOLDER
+	NSString *path = [NSHomeDirectory() stringByAppendingFormat:@"/Documents/%@", folderName];
+	BOOL isDir;
+	BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir];
+	
+	if (exists && isDir) {
+		// HAS FOLDER
+		// DO NOTHING
+	} else {
+		// CREATE
+		[[NSFileManager defaultManager] createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
+	}
 }
 
 @end
