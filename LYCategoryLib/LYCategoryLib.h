72cf865bca2b58427c65a1f27422efd79d2194e5
Luo Yu <indie.luo@gmail.com>
2014-11-26 17:38:53 +0800

    create static lib project.

---
+++ LYCategoryLib/LYCategoryLib.h
@@ -0,0 +1,13 @@
+//
+//  LYCategoryLib.h
+//  LYCategoryLib
+//
+//  Created by Luo Yu on 2014-11-26.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYCategoryLib : NSObject
+
+@end
---
+++ LYCategoryLib/LYCategoryLib.m
@@ -0,0 +1,13 @@
+//
+//  LYCategoryLib.m
+//  LYCategoryLib
+//
+//  Created by Luo Yu on 2014-11-26.
+//  Copyright (c) 2014 Luo Yu. All rights reserved.
+//
+
+#import "LYCategoryLib.h"
+
+@implementation LYCategoryLib
+
+@end
