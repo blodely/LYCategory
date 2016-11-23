af65d3799dd298565999503f80c072d16bd9e1aa
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2016-11-23 14:12:50 +0800

    Modify : md5 uppercase 32

--- LYCategory/Classes/_Foundation/_Crypto/NSString+Encrypt.m
+++ LYCategory/Classes/_Foundation/_Crypto/NSString+Encrypt.m
@@ -135,7 +135,16 @@ static Byte iv[] = {0x12,0x34,0x56,0x78,0x90,0xAB,0xCD,0xEF};
 
 - (NSString *)md5Uppercase32 {
 	
-	return nil;
+	const char *cStr = [self UTF8String];
+	unsigned char result[32];
+	
+	CC_MD5(cStr, (unsigned int)strlen(cStr), result);
+	return [NSString stringWithFormat:
+			@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+			result[0],result[1],result[2],result[3],
+			result[4],result[5],result[6],result[7],
+			result[8],result[9],result[10],result[11],
+			result[12],result[13],result[14],result[15]];
 }
 
 @end
