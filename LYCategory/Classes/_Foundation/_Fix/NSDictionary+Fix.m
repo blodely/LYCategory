c7c410e42bcd9ca62e1364124fb4d74b210b2992
骆昱 (Mac mini) <indie.luo@gmail.com>
2016-12-16 14:57:20 +0800

    Modify : dictionary log

--- LYCategory/Classes/_Foundation/_Fix/NSDictionary+Fix.m
+++ LYCategory/Classes/_Foundation/_Fix/NSDictionary+Fix.m
@@ -189,4 +189,17 @@
 	return self.allKeys.count > 0;
 }
 
+// OVERWRITE FOR HANS LOG
+- (NSString *)descriptionWithLocale:(id)locale {
+	NSMutableString *strM = [NSMutableString stringWithString:@"{\n"];
+	
+	[self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+		[strM appendFormat:@"\t%@ = %@;\n", key, obj];
+	}];
+	
+	[strM appendString:@"}\n"];
+	
+	return strM;
+}
+
 @end
