7c08d5bf36aa92515d77e1ee20fc740a03d9f3d3
骆昱 (Mac mini) <indie.luo@gmail.com>
2017-03-15 14:16:06 +0800

    [Modify] doc

--- LYCategory/Classes/_Foundation/_Create/NSString+Date.h
+++ LYCategory/Classes/_Foundation/_Create/NSString+Date.h
@@ -18,6 +18,13 @@
  */
 - (NSDate *)dateWithFormat:(NSString *)fmtString;
 
+/**
+ 字符串转换时间对象
+
+ @param fmtString 转换格式
+ @param timezoneName 时区标识符
+ @return 时间对象结果
+ */
 - (NSDate *)dateWithFormat:(NSString *)fmtString andTimezone:(NSString *)timezoneName;
 
 - (NSDate *)dateWithFormat:(NSString *)fmtString dateStyle:(NSDateFormatterStyle)style;
