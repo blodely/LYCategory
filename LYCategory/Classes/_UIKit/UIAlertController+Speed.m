855e5e077f0262a4a6503003ae8d3c304d00ecf9
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-03-24 14:02:38 +0800

    [Modify] implemented

--- LYCategory/Classes/_UIKit/UIAlertController+Speed.m
+++ LYCategory/Classes/_UIKit/UIAlertController+Speed.m
@@ -34,4 +34,22 @@
 	[viewvc presentViewController:alert animated:YES completion:^{}];
 }
 
++ (void)showAlertFromView:(UIViewController *)viewvc
+				withTitle:(NSString *)title
+			   andMessage:(NSString *)message
+		cancelButtonTitle:(NSString *)btnCancelTitle
+			 cancelAction:(void (^)(void))btnCancelBlock {
+	
+	// CREATION
+	UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+	
+	// CANCEL BUTTON
+	[alert addAction:[UIAlertAction actionWithTitle:btnCancelTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+		btnCancelBlock();
+	}]];
+	
+	// SHOW
+	[viewvc presentViewController:alert animated:YES completion:^{}];
+}
+
 @end
