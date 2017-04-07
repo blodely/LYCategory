0d1323fb285dd387463a8fded84752157580434e
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-04-07 14:18:52 +0800

    [Modify] doc

--- LYCategory/Classes/_CoreLocation/CLLocation+Speed.h
+++ LYCategory/Classes/_CoreLocation/CLLocation+Speed.h
@@ -12,13 +12,19 @@
 @interface CLLocation (Speed)
 
 /**
- coordinate converter: from b-a-i-d-u GPS to GCJ-02 GPS
+ coordinate converter: from Bai-du GPS to GCJ-02 GPS
 
  @param coordinateBd Bai-du GPS coordinate
  @return GCJ-02 coordinate
  */
 + (CLLocationCoordinate2D)convertFromBDToGCJ02:(CLLocationCoordinate2D)coordinateBd;
 
+/**
+ coodinate converter: from GCJ-02 GPS to Bai-du GPS
+
+ @param coordinateGCJ02 GCJ-02 coordinate
+ @return Bai-du GPS coordinate
+ */
 + (CLLocationCoordinate2D)convertFromGCJ02ToBD:(CLLocationCoordinate2D)coordinateGCJ02;
 
 + (CLLocationCoordinate2D)convertFromWGS84ToGCJ02:(CLLocationCoordinate2D)coordinateWGS84;
