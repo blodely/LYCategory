e124b432075f79669ac157f8dc09f3aba470e9dd
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-05-22 08:15:53 +0800

    [Modify] hex color implementation

--- LYCategory/Classes/_UIKit/UIColor+Speed.m
+++ LYCategory/Classes/_UIKit/UIColor+Speed.m
@@ -25,7 +25,46 @@
 }
 
 + (UIColor *)colorWithHex:(NSString *)hexstring andAlpha:(CGFloat)alpha {
-	return nil;
+	if (hexstring.length == 0) {
+		return [UIColor clearColor];
+	}
+	
+	if('#' != [hexstring characterAtIndex:0]) {
+		hexstring = [NSString stringWithFormat:@"#%@", hexstring];
+	}
+	
+	// RETURNING NO OBJECT ON WRONG ALPHA VALUES
+	NSArray *validHexStringLengths = @[@7,];
+	NSNumber *hexStringLengthNumber = [NSNumber numberWithUnsignedInteger:hexstring.length];
+	if ([validHexStringLengths indexOfObject:hexStringLengthNumber] == NSNotFound) {
+		return nil;
+	}
+	
+	unsigned value = 0;
+	NSScanner *hexValueScanner = nil;
+	
+	NSString *redHex = [NSString stringWithFormat:@"0x%@", [hexstring substringWithRange:NSMakeRange(1, 2)]];
+	hexValueScanner = [NSScanner scannerWithString:redHex];
+	[hexValueScanner scanHexInt:&value];
+	unsigned redInt = value;
+	hexValueScanner = nil;
+	
+	NSString *greenHex = [NSString stringWithFormat:@"0x%@", [hexstring substringWithRange:NSMakeRange(3, 2)]];
+	hexValueScanner = [NSScanner scannerWithString:greenHex];
+	[hexValueScanner scanHexInt:&value];
+	unsigned greenInt = value;
+	hexValueScanner = nil;
+	
+	NSString *blueHex = [NSString stringWithFormat:@"0x%@", [hexstring substringWithRange:NSMakeRange(5, 2)]];
+	hexValueScanner = [NSScanner scannerWithString:blueHex];
+	[hexValueScanner scanHexInt:&value];
+	unsigned blueInt = value;
+	hexValueScanner = nil;
+	
+	alpha = MIN(alpha, 1.0f);
+	alpha = MAX(0, alpha);
+	
+	return [UIColor colorWithRed:redInt/255.0f green:greenInt/255.0f blue:blueInt/255.0f alpha:alpha];
 }
 
 @end
