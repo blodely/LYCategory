071c7dd763115a5eeeba9608193ee2b12c27e778
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-07-17 17:12:56 +0800

    [Add] constants

---
+++ LYCategory/Classes/_Const/LYCategoryConstants.h
@@ -0,0 +1,14 @@
+//
+//  LYCategoryConstants.h
+//  LYCategory
+//
+//  Created by Luo Yu on 2017-07-18.
+//  Copyright (c) 2017 Luo Yu. All rights reserved.
+//  Email: indie.luo@gmail.com
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYCategoryConstants : NSObject
+
+@end
---
+++ LYCategory/Classes/_Const/LYCategoryConstants.m
@@ -0,0 +1,14 @@
+//
+//  LYCategoryConstants.m
+//  LYCategory
+//
+//  Created by Luo Yu on 2017-07-18.
+//  Copyright (c) 2017 Luo Yu. All rights reserved.
+//  Email: indie.luo@gmail.com
+//
+
+#import "LYCategoryConstants.h"
+
+@implementation LYCategoryConstants
+
+@end
