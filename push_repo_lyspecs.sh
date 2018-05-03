9559548c838d907da880ec9e79fa4438064a799d
Luo Yu <luoyu@luoyu.space>
2018-05-03 12:02:47 +0800

    [Add] push to repo script

---
+++ push_repo_lyspecs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#  push_repo_lyspecs.sh
+#  LYCategory
+#
+#  Created by Luo Yu on 03/05/2018.
+#  Copyright © 2018 骆昱 (indie.luo@gmail.com). All rights reserved.
+
+
+pod repo push LYSpecs LYCategory.podspec --allow-warnings
