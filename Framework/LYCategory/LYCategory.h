919225a0d90b0e4ae8506d73db738b2bae48ffac
Luo Yu <luoyu@luoyu.space>
2018-08-06 10:03:08 +0800

    [Add] project framework

---
+++ Framework/LYCategory/LYCategory.h
@@ -0,0 +1,97 @@
+//
+//  LYCategory.h
+//  LYCategory
+//
+//  Created by Luo Yu on 7/3/13.
+//	Email: indie.luo@gmail.com
+//
+//	The MIT License (MIT)
+//
+//	Copyright © 2013 骆昱(Luo Yu). All rights reserved.
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy of
+//	this software and associated documentation files (the "Software"), to deal in
+//	the Software without restriction, including without limitation the rights to
+//	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//	the Software, and to permit persons to whom the Software is furnished to do so,
+//	subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in all
+//	copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//	CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for LYCategory.
+FOUNDATION_EXPORT double LYCategoryVersionNumber;
+
+//! Project version string for LYCategory.
+FOUNDATION_EXPORT const unsigned char LYCategoryVersionString[];
+
+// IMPORT UTILITIES
+
+#import <LYCategory/NSData+Encrypt.h>
+
+#import <LYCategory/NSString+Encrypt.h>
+#import <LYCategory/NSString+Input.h>
+#import <LYCategory/NSString+Speed.h>
+#import <LYCategory/NSString+Size.h>
+#import <LYCategory/NSString+Date.h>
+#import <LYCategory/NSString+Search.h>
+#import <LYCategory/NSString+Random.h>
+#import <LYCategory/NSString+Convert.h>
+
+#import <LYCategory/NSNumber+Random.h>
+#import <LYCategory/NSNumber+Date.h>
+#import <LYCategory/NSNumber+Distance.h>
+
+#import <LYCategory/NSDate+Formatter.h>
+#import <LYCategory/NSDate+Operator.h>
+
+#import <LYCategory/NSLayoutConstraint+APixel.h>
+
+// ARRAY
+#import <LYCategory/NSArray+Operator.h>
+#import <LYCategory/NSMutableArray+Operator.h>
+
+#import <LYCategory/NSURL+Speed.h>
+#import <LYCategory/NSURLRequest+Speed.h>
+
+#import <LYCategory/NSUserDefaults+Speed.h>
+
+// FILE
+#import <LYCategory/NSFileManager+IOS.h>
+#import <LYCategory/NSFileManager+Size.h>
+
+// FIX
+#import <LYCategory/NSArray+Fix.h>
+#import <LYCategory/NSDictionary+Fix.h>
+#import <LYCategory/NSNumber+Fix.h>
+#import <LYCategory/NSNull+Fix.h>
+#import <LYCategory/NSMutableArray+Fix.h>
+#import <LYCategory/NSMutableDictionary+Fix.h>
+
+#import <LYCategory/MacroIOS.h>
+#import <LYCategory/LYCategoryConstants.h>
+
+// UI
+#import <LYCategory/UITextField+Input.h>
+#import <LYCategory/UITextView+Input.h>
+#import <LYCategory/UIView+Screenshot.h>
+#import <LYCategory/UIImage+Scale.h>
+#import <LYCategory/UIImage+Filter.h>
+#import <LYCategory/UIView+Animate.h>
+#import <LYCategory/UIColor+Speed.h>
+#import <LYCategory/UINavigationBar+Fix.h>
+#import <LYCategory/UIAlertController+Speed.h>
+#import <LYCategory/UIView+Speed.h>
+
+#import <LYCategory/CLLocation+Speed.h>
