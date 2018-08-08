cea32a7f72a9dae1eb99f4ea5c97cbaa151a7733
Luo Yu <luoyu@luoyu.space>
2018-08-08 14:58:57 +0800

    [Add] push to trunk script

---
+++ push_to_trunk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#  push_repo_lyspecs.sh
+#  LYCategory
+#
+#  Created by Luo Yu on 08/08/2018.
+#  Copyright © 2018 骆昱 (indie.luo@gmail.com). All rights reserved.
+
+# print my profile
+pod trunk me
+
+# push to trunk
+pod trunk push
